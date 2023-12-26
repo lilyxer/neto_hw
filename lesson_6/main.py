d312197ad9f5149777c5a5b2bee873f83fdf1163
Михаил Борисов <borisov.uk@gmail.com>
2023-12-26 18:22:37 +0300

    рефакторинг кода, прописал тесты, классы отправил в отдельный модуль

---
+++ lesson_6/main.py
@@ -0,0 +1,113 @@
+from my_modules.my_classes import *
+from random import randint, choice, seed
+
+
+# наполняем данными
+def run():
+    def students_eval(stud: Student, lect: Lecturer):
+        """
+        функция рандомно проставялет оценки лекторам от студентов.
+        курсы должны совпадать
+        результат выводится на печать и валидные оценки попадают в поле экземпляра
+        """
+        print(f'общие курсы у {stud.name} и {lect.name}: ', end='')
+        print(*set(stud.courses_in_progress)&set(lect.courses_attached), sep=', ')
+        for _ in range(20):
+            stud.rate_lecturer(lecturer=lect, course=choice(all_courses),
+                                grade=randint(1, 10))
+        print(f'проверим правильность заполнения\n'
+               f'Оценки лектора {lect.surname}\n'
+               f'{lect.grades_from_stud}\n'
+               f'{"-"*80}')
+
+    def reviewer_eval(rev: Reviewer, stud: Student):
+        """
+        функция рандомно проставялет оценки студентам от экспертов.
+        экспрет может оценивать все курсы 
+        результат выводится на печать и валидные оценки попадают в поле экземпляра
+        """
+        print(f'общие курсы у {stud.name} и {rev.name}: ', end='')
+        print(*set(rev.courses_attached)&set(stud.courses_in_progress), sep=', ')
+        for _ in range(20):
+            rev.rate_hw(student=stud, course=choice(all_courses), 
+                        grade=randint(1, 10))
+        print(f'проверим правильность заполнения\n'
+              f'Оценки студента {stud.surname}\n'
+              f'{stud.grades}\n'
+              f'{"-"*80}')
+
+    def negative_check():
+        """
+        тесты на проверку
+        """
+        assert (student_1.rate_lecturer(lecturer=lecturer_1, 
+                                        course='Python', grade=1) 
+                                        == 'Python не является общим')
+        
+        assert ((student_1.rate_lecturer(lecturer=lecturer_1, 
+                                         course='GO', grade=20)) 
+                                         == '20 не является валидной оценкой')
+        
+        assert ((student_1.rate_lecturer(lecturer=lecturer_1, 
+                                         course='GO', grade='2')) 
+                                         == '2 не является валидной оценкой')
+        
+        assert ((student_1.rate_lecturer(lecturer=reviewer_1, 
+                                         course='GO', grade=2)) 
+                                         == 'Reviewerova не явлется лектором')
+        
+        assert ((reviewer_1.rate_hw(student=student_1, 
+                                    course='GO', grade='1')) 
+                                    == '1 не является валидной оценкой')
+        
+        assert (reviewer_1.rate_hw(student=student_1, 
+                                    course='Python', grade='1') 
+                                    == 'Python не является общим')
+
+        assert (reviewer_1.rate_hw(student=lecturer_1, 
+                                    course='SQL', grade=1)
+                                    == 'Lectorova не явлется студентом')
+
+    seed(100) # данные будут постоянны
+
+    # создаем по 2 экземпляра: студента, лектора, эксперта
+    student_1 = Student(name='Alexandra', surname='Studentova', gender='Female')
+    student_2 = Student(name='Alexandr', surname='Studentov', gender='Male')
+    lecturer_1 = Lecturer(name='Valeriya', surname='Lectorova')
+    lecturer_2 = Lecturer(name='Valeriy', surname='Lectorov')
+    reviewer_1 = Reviewer(name='Evgeniya', surname='Reviewerova')
+    reviewer_2 = Reviewer(name='Evgeniy', surname='Reviewerov')
+
+    # заполняем поля созданных экземпляров
+    all_courses = ['GO', 'SQL', 'Git', 'JavaSript', 'Python']
+    student_1.finished_courses.extend(['Git', 'CSS', 'HTML'])
+    student_2.finished_courses.extend(['Основы программирования'])
+
+    student_1.courses_in_progress.extend(['GO', 'SQL', 'JavaScript'])
+    student_2.courses_in_progress.extend(['Python', 'Git', 'SQL'])
+
+    lecturer_1.courses_attached.extend(['GO', 'SQL'])
+    lecturer_2.courses_attached.extend(['Python', 'Git', 'JavaSript'])
+
+    reviewer_1.courses_attached.extend(all_courses)
+    reviewer_2.courses_attached.extend(all_courses)
+
+    # ставим оценки преподавателям
+    students_eval(stud=student_1, lect=lecturer_1)
+    students_eval(stud=student_1, lect=lecturer_2)
+    students_eval(stud=student_2, lect=lecturer_1)
+    students_eval(stud=student_2, lect=lecturer_2)
+
+    # ставим оценки студентам
+    reviewer_eval(stud=student_1, rev=reviewer_1)
+    reviewer_eval(stud=student_1, rev=reviewer_2)
+    reviewer_eval(stud=student_2, rev=reviewer_1)
+    reviewer_eval(stud=student_2, rev=reviewer_2)
+
+    # тесты на негативные проверки
+    negative_check()
+
+
+if __name__ == '__main__':
+    run()
+    
--- lesson_6/my_modules/my_classes.py
+++ lesson_6/my_modules/my_classes.py
@@ -1,13 +1,4 @@
 from functools import total_ordering
-from random import randint, choice, seed
-
-
-class CourseError(BaseException):
-    ...
-
-
-class GradeError(BaseException):
-    ...
 
 
 class Mentor:
@@ -52,12 +43,12 @@ class Lecturer(Mentor):
     def __eq__(self, other) -> bool:
         if isinstance(other, self(type)): 
             return self._get_all_grades() == other._get_all_grades()
-        raise NotImplemented
+        return NotImplemented
 
     def __lt__(self, other) -> bool:
         if isinstance(other, self(type)): 
             return self._get_all_grades() < other._get_all_grades()
-        raise NotImplemented
+        return NotImplemented
 
 
 @total_ordering 
@@ -78,7 +69,7 @@ class Student:
         self.courses_in_progress = []
         self.grades = {}
 
-    def rate_lecturer(self, lecturer: Lecturer, course: str, grade: int) -> None:
+    def rate_lecturer(self, lecturer: Lecturer, course: str, grade: int) -> bool|str:
         """
         метод студента, выставляет оценку лектору по общему курсу
         lecturer - должен быть class Lecturer
@@ -91,9 +82,9 @@ class Student:
                 if isinstance(grade, int) and grade in range(1, 11):
                     lecturer.grades_from_stud.setdefault(course, []).append(grade)
                     return True
-                raise GradeError(f'{grade} не является валидной оценкой')
-            raise CourseError(f'{course} не является общим')
-        raise TypeError(f'{lecturer.surname} не явлется лектором')
+                return f'{grade} не является валидной оценкой'
+            return f'{course} не является общим'
+        return f'{lecturer.surname} не явлется лектором'
 
 
     def _get_all_grades(self) -> float|None:
@@ -122,16 +113,16 @@ class Student:
     def __eq__(self, other) -> bool:
         if isinstance(other, self(type)): 
             return self._get_all_grades() == other._get_all_grades()
-        raise NotImplemented
+        return NotImplemented
 
     def __lt__(self, other) -> bool:
         if isinstance(other, self(type)): 
             return self._get_all_grades() < other._get_all_grades()
-        raise NotImplemented
+        return NotImplemented
     
 
 class Reviewer(Mentor):
-    def rate_hw(self, student: Student, course: str, grade: int) -> None:
+    def rate_hw(self, student: Student, course: str, grade: int) -> bool|str:
         """
         метод Ментора, выставляет оценку студенту
         student - должен быть class Student
@@ -144,90 +135,9 @@ class Reviewer(Mentor):
                 if isinstance(grade, int) and grade in range(1, 11):
                     student.grades.setdefault(course, []).append(grade)
                     return True
-                raise GradeError(f'{grade} не является валидной оценкой')
-            raise CourseError(f'{course} не является общим')
-        raise TypeError(f'{student.surname} не явлется студентом')
+                return f'{grade} не является валидной оценкой'
+            return f'{course} не является общим'
+        return f'{student.surname} не явлется студентом'
 
     def __str__(self) -> str:
         return f'Имя: {self.name}\nФамилия: {self.surname}'
-
-
-def students_eval(stud: Student, lect: Lecturer):
-    """
-    функция рандомно проставялет оценки лекторам от студентов.
-    курсы должны совпадать
-    результат выводится на печать и валидные оценки попадают в поле экземпляра
-    """
-    print(f'общие курсы у {stud.name} и {lect.name}: ', end='')
-    print(*set(stud.courses_in_progress)&set(lect.courses_attached))
-    for _ in range(20):
-        try:
-            stud.rate_lecturer(lecturer=lect, course=choice(all_courses),
-                                grade=randint(1, 10))
-        except (TypeError, GradeError, CourseError): 
-            pass
-    print('проверим правильность заполнения')
-    print(lect.grades_from_stud)
-    print('-'*80)
-    print(f'проверим правильность заполнения\n'
-            f'Оценки лектора {lect.surname}\n'
-            f'{lect.surngrades_from_studame}\n'
-            f'{"-"*80}')
-
-def reviewer_eval(rev: Reviewer, stud: Student):
-    """
-    функция рандомно проставялет оценки студентам от экспертов.
-    экспрет может оценивать все курсы 
-    результат выводится на печать и валидные оценки попадают в поле экземпляра
-    """
-    print(f'общие курсы у {stud.name} и {rev.name}: ', end='')
-    print(*set(rev.courses_attached)&set(stud.courses_in_progress))
-    for _ in range(20):
-        try:
-            rev.rate_hw(student=stud, course=choice(all_courses), 
-                        grade=randint(1, 10))
-        except (TypeError, GradeError, CourseError): 
-            pass            
-    print(f'проверим правильность заполнения\n'
-        f'Оценки студента {stud.surname}\n'
-        f'{stud.grades}\n'
-        f'{"-"*80}')
-
-if __name__ == '__main__':
-    seed(100) # данные будут постоянны
-
-    # создаем по 2 экземпляра: студента, лектора, эксперта
-    student_1 = Student(name='Alexandra', surname='Studentova', gender='Female')
-    student_2 = Student(name='Alexandr', surname='Studentov', gender='Male')
-    lecturer_1 = Lecturer(name='Valeriya', surname='Lectorova')
-    lecturer_2 = Lecturer(name='Valeriy', surname='Lectorov')
-    reviewer_1 = Reviewer(name='Evgeniya', surname='Reviewerova')
-    reviewer_2 = Reviewer(name='Evgeniy', surname='Reviewerov')
-
-    # заполняем поля созданных экземпляров
-    all_courses = ['GO', 'SQL', 'Git', 'JavaSript', 'Python']
-    student_1.finished_courses.extend(['Git', 'CSS', 'HTML'])
-    student_2.finished_courses.extend(['Основы программирования'])
-
-    student_1.courses_in_progress.extend(['GO', 'SQL', 'JavaScript'])
-    student_2.courses_in_progress.extend(['Python', 'Git', 'SQL'])
-
-    lecturer_1.courses_attached.extend(['GO', 'SQL'])
-    lecturer_2.courses_attached.extend(['Python', 'Git', 'JavaSript'])
-
-    reviewer_1.courses_attached.extend(all_courses)
-    reviewer_2.courses_attached.extend(all_courses)
-
-    # ставим оценки преподавателям
-    students_eval(stud=student_1, lect=lecturer_1)
-    students_eval(stud=student_1, lect=lecturer_2)
-    students_eval(stud=student_2, lect=lecturer_1)
-    students_eval(stud=student_2, lect=lecturer_2)
-
-    # ставим оценки студентам
-    reviewer_eval(stud=student_1, rev=reviewer_1)
-    reviewer_eval(stud=student_1, rev=reviewer_2)
-    reviewer_eval(stud=student_2, rev=reviewer_1)
-    reviewer_eval(stud=student_2, rev=reviewer_2)
-
-    
